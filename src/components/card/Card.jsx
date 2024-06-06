fa9c4d70a580ecf064175e807245166b5bf3d10c
Thierry De Sousa <elanion2007@gmail.com>
2024-06-06 21:55:19 +0200

    updating the 'mesRealisations' view

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,36 +2,39 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
+// Components
+import IconReact from '../iconReact/IconReact.jsx'
+
 // Style
 import './card.css'
 
 function Card(props) {
 
     return (
-        <article className="containerCard">
-            <div className="containerCard__work">
-                <img src={props.src} alt={props.alt} className='containerCard__work--img' />
-            </div>
-            <div className="containerCard__title"> 
+        <div className='work'>      
+            <div className='work__containerImg'>
+                <img src={props.src} alt={props.alt} className='work__containerImg--img'/>
+            </div>   
+            <div className='work__containerLienTitle'>
                 <a href={props.href} 
                     target="_blank" 
                     rel="noopener noreferrer"
-                    aria-label="Lien vers mon Github"
-                    className='containerCard__title--lien'
+                    aria-label="Lien vers mon projet Github"
+                    className='work__containerLienTitle--lien'
                 >
                     <FontAwesomeIcon 
-                            icon={faGithub} 
-                            size="2x" 
-                        />
+                        icon={faGithub} 
+                        size="2x" 
+                        className='iconGitHubWork'
+                    />
                 </a>
-                <h2>{props.title}</h2> 
+                <h2>{props.title}</h2>
             </div>
-            <div className="containerCard__texte">
-                <div className="likes">
-                    <svg viewBox="-2 0 105 92" className="likes_svg"><path d="M85.24 2.67C72.29-3.08 55.75 2.67 50 14.9 44.25 2 27-3.8 14.76 2.67 1.1 9.14-5.37 25 5.42 44.38 13.33 58 27 68.11 50 86.81 73.73 68.11 87.39 58 94.58 44.38c10.79-18.7 4.32-35.24-9.34-41.71Z"></path></svg><span className="likes_text">22</span>
-                </div>
+            <div className='work__containerIconText'>
+                <IconReact />
+                <p className='work__containerIconText--text'>{props.text}</p>
             </div>
-        </article>
+        </div>
     );
 }
 
--- src/pages/mesRealisations/MesRealisations.jsx
+++ src/pages/mesRealisations/MesRealisations.jsx
@@ -6,6 +6,9 @@ import Card from '../../components/card/Card.jsx'
 
 // Variables
 import argentBank from '../../img/ArgentBank.webp'
+import works from '../../datas/works.json';
+//import leftArrow from '../../img/leftArrow.png';
+//import rightArrow from '../../img/rightArrow.png';
 
 // Style
 import './mesRealisations.css'
@@ -17,18 +20,20 @@ function MesRealisations() {
         setVisibleClass('fade visible');
     }, []);
 
+    const argentBankWork = works.find(work => work.id === "argentBank" ); 
+
     return (
         <main>
-            <section>
-                <div className={`containerMesRealisations ${visibleClass}`}>
-                    <Card 
-                        src={argentBank}
-                        alt="image du projet Argent Bank"
-                        title="Argent Bank" 
-                        href="https://github.com/Thierry77170/argentBank.git"
-                    />
-                </div>
-            </section>
+            <section className={`containerMesRealisations ${visibleClass}`}>
+                <Card 
+                    src={argentBank}
+                    alt="image du projet Argent Bank"
+                    href={argentBankWork.lien}
+                    title={argentBankWork.title}
+                    text={argentBankWork.text}         
+                    className="containerMesRealisations__work"          
+                 />
+            </section>  
         </main>
     );
 }
